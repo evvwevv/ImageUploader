aad0a113eea8b606e8d8f83b06be854dde98ae8c
Steven Johnson <sjsushiman@gmail.com>
2018-10-15 10:04:46 +0900

    Update app component test spec

--- angularFrontend/src/app/app.component.spec.ts
+++ angularFrontend/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        CoreModule,
+        SharedModule
       ],
       declarations: [
         AppComponent
@@ -26,10 +30,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angularFrontend');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angularFrontend!');
-  });
 });
