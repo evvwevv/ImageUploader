7b443dd086ccfefbebae2380bfb83e0b4d929c78
Steven Johnson <sjsushiman@gmail.com>
2018-10-15 10:22:13 +0900

    Update login component test spec

--- angularFrontend/src/app/login/login.component.spec.ts
+++ angularFrontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [
+        CoreModule,
+        SharedModule
+      ],
+      declarations: [ 
+        LoginComponent 
+      ]
     })
     .compileComponents();
   }));
