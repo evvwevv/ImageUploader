d24f379442b097a97bb05dd6a08563507523cd9e
Steven Johnson <sjsushiman@gmail.com>
2018-12-04 10:22:44 +0900

    Add dropzone dependencies to project

--- angularFrontend/src/app/shared/shared.module.ts
+++ angularFrontend/src/app/shared/shared.module.ts
@@ -6,7 +6,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropzoneModule } from 'ngx-dropzone-wrapper';
+import { DROPZONE_CONFIG } from 'ngx-dropzone-wrapper';
+import { DropzoneConfigInterface } from 'ngx-dropzone-wrapper';
 
+const DEFAULT_DROPZONE_CONFIG: DropzoneConfigInterface = {
+    // Change this to your upload POST address:
+     url: 'https://httpbin.org/post',
+     maxFilesize: 50,
+     acceptedFiles: 'image/*'
+   };
 
 @NgModule({
     imports: [
