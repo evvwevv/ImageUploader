06c4b44cefbb583c66607f519fdb8adfffbf7912
Steven Johnson <sjsushiman@gmail.com>
2018-12-06 10:09:26 +0900

    Add error handling to login page

--- angularFrontend/src/app/login/login.component.ts
+++ angularFrontend/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from './../auth/auth.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import {ErrorDialogComponent} from '../home/home.component';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +14,26 @@ export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
   hide = true;
   email = new FormControl('', [Validators.required, Validators.email]);
+  errorDialogRef: MatDialogRef<ErrorDialogComponent>;
+  errorTitle = 'Sign in Error';
+  errorMsg = 'We were unable to sign you in, please make sure your email and password are correct.';
 
   constructor(private fb: FormBuilder,
               private router: Router,
-              private auth: AuthService) {
+              private auth: AuthService,
+              public dialog: MatDialog) {
   }
 
   ngOnInit() {
     this.initForm();
+  }
+
+  openErrorDialog(errorMsg: string, errorTitle: string): void {
+    this.dialog.open(ErrorDialogComponent, {
+      width: '450px',
+      autoFocus: true,
+      data: {errorMsg: errorMsg, errorTitle: errorTitle}
+    });
   }
 
   initForm() {
@@ -37,6 +51,7 @@ export class LoginComponent implements OnInit {
         this.router.navigate(['/']);
       },
       error => {
+        this.openErrorDialog(this.errorMsg, this.errorTitle);
         console.log(error);
       }
     );
