54dcaf81252b7a9895f2fa15ae0c98894fdaaca5
Steven Johnson <sjsushiman@gmail.com>
2019-01-07 13:23:27 -0500

    Add gallery component to router

--- angularFrontend/src/app/app-routing.module.ts
+++ angularFrontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {SignupComponent} from './signup/signup.component';
 import {HomeComponent} from './home/home.component';
+import {GalleryComponent} from './gallery/gallery.component';
 import {AuthGuard} from './auth/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent}
+  {path: 'signup', component: SignupComponent},
+  {path: 'gallery', component: GalleryComponent}
 
 ];
 
