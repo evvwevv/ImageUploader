278d278707a3a83a5392407e76580f00d489b00e
wileong <wilson.leong.0@gmail.com>
2019-03-22 00:23:28 -0700

    Added sample lambda function with the necessary Python libraries for future reference

---
+++ backendResources/lambdaFunctions_Backups/SAMPLE_Lambda_Function_With_Nececessary_Python_Libraries/rds_mysql_config.py
@@ -0,0 +1,5 @@
+#config file containing credentials for rds mysql instance
+# actual username and password was not included for security purposes
+db_username = "HIDDEN"
+db_password = "HIDDEN"
+db_name = "imageuploader_webapp_db"
